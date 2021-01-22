763051cdd90c9c57bca6f6da8194ea5ce0c4a506
april <apriltopolewski@gmail.com>
2021-01-21 20:13:54 -0500

    created components and \configured routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
+import { HomeComponent } from './home/home.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', component: HomeComponent },
+{path: 'Recipes', component: RecipeListComponent},
+{path: 'Favorites', component: FavoritesPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { FavoritesPageComponent } from './favorites-page/favorites-page.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    RecipeListComponent,
+    FavoritesPageComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
