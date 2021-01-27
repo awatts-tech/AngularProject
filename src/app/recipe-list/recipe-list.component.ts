789f817bebba20665ff628c9f121839bc420aac3
april <apriltopolewski@gmail.com>
2021-01-26 20:00:45 -0500

    added more info in the recipe list component for better searchability

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipesApiService } from '../recipes-api.service';
 
+interface Recipe{	
+  // insert data wanted from the API here	
+    //title	
+    //thumbnail	
+    //link	
+    //short description?	
+  }
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  //recipe:Recipe;
   constructor(public recipeAPI: RecipesApiService) { }
 
   ngOnInit(): void {
     this.recipeAPI.getRecipes();
   }
 
-  
+
 
 }
